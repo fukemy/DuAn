144ae5b27b5d5e72feecd80e2bf1e6295a0f2bd8
dungdv4@fsoft.@fsoft.com.vn <dungdv4@fsoft.com.vn>
2016-08-20 15:02:35 +0700

    fap

--- app/src/main/java/com/example/macos/fragment/report/FragmentReportStatus.java
+++ app/src/main/java/com/example/macos/fragment/report/FragmentReportStatus.java
@@ -514,24 +514,23 @@ public class FragmentReportStatus extends CustomFragment {
         hashMap = new HashMap<>();
         listHeader=  new ArrayList<>();
         for(EnDataModel en : data){
-            if(en.getDaValue() != null) {
-                Logger.error("fapfap:" + en.getDaValue().toString());
-                if(en.getDaValue().getAction().equals(getResources().getString(R.string.accident)) || en.getDaValue().getAction().equals(getResources().getString(R.string.problem))
-                        || en.getDaValue().getAction().equals(getResources().getString(R.string.lastday))) {
+            if(en.getDaValue().getAction().equals(getResources().getString(R.string.report )) ||
+                    en.getDaValue().getAction().equals(getResources().getString(R.string.accident)) ||
+                    en.getDaValue().getAction().equals(getResources().getString(R.string.problem)) ||
+                    en.getDaValue().getAction().equals(getResources().getString(R.string.lastday))) {
+            }else{
+                if(!hashMap.containsKey(en.getDaValue().getDataName())){
+                    List<EnDataModel> statusList = new ArrayList<>();
+                    statusList.add(en);
+                    listHeader.add(en.getDaValue().getDataName());
+                    hashMap.put(en.getDaValue().getDataName(), statusList);
                 }else{
-                    if(!hashMap.containsKey(en.getDaValue().getDataName())){
-                        List<EnDataModel> statusList = new ArrayList<>();
-                        statusList.add(en);
-                        listHeader.add(en.getDaValue().getDataName());
-                        hashMap.put(en.getDaValue().getDataName(), statusList);
-                    }else{
-                        Iterator myVeryOwnIterator = hashMap.keySet().iterator();
-                        while(myVeryOwnIterator.hasNext()) {
-                            String key = (String) myVeryOwnIterator.next();
-                            if (key.equals(en.getDaValue().getDataName())) {
-                                List<EnDataModel> listCurrently = hashMap.get(key);
-                                    listCurrently.add(en);
-                            }
+                    Iterator myVeryOwnIterator = hashMap.keySet().iterator();
+                    while(myVeryOwnIterator.hasNext()) {
+                        String key = (String) myVeryOwnIterator.next();
+                        if (key.equals(en.getDaValue().getDataName())) {
+                            List<EnDataModel> listCurrently = hashMap.get(key);
+                                listCurrently.add(en);
                         }
                     }
                 }
